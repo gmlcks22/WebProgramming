9dd8d353e0c245d798ec8c4af0dd3c541d5e2ae1
gmlcks <benny@hansung.ac.kr>
2024-11-18 20:11:29 +0900

    yes indeed

--- practice/arrow-function.js
+++ practice/arrow-function.js
@@ -5,7 +5,7 @@ const plus = (a, b) => a + b;
 const minus = (a, b) => a - b;
 
 printHello();
-console.log(printHello2);
+console.log(printHello2());
 printMessage('message');
 console.log(`plus : ${plus(10, 20)}`);
 console.log(`minus : ${minus(20, 10)}`);
